80800bdfbba229b8baf5877d9025fe56cc6e35e3
m.shkolin <m.shkolin5640@gmail.com>
2023-12-31 15:57:57 +0500

    cleanup (test commit)

--- EmailService/Server/Models/CreateMailDto.cs
+++ EmailService/Server/Models/CreateMailDto.cs
@@ -1,12 +1,7 @@
 ﻿using AutoMapper;
 using EmailSender.Application.Common.Mappings;
 using EmailSender.Application.Mails.Commands.CreateMail;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmailSender.Server.Models
 {
